1fbd39ebee1556a1a4c06b47c006a4d663189ef8
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2022-04-19 11:45:41 +0800

    finnished stats

--- src/components/FeedbackStats/FeedbackStats.jsx
+++ src/components/FeedbackStats/FeedbackStats.jsx
@@ -5,13 +5,15 @@ import { FeedbackContext } from '../../context/FeedbackContext';
 function FeedbackStats() {
 
     const { feedbacks } = useContext(FeedbackContext);
-
-
+    const avgRating = (feedbacks.reduce((state, feedback)=>(
+        state + feedback.rating
+    ), 0) / feedbacks.length).toFixed(0);
+   
 
   return (
     <Container sx={{display: "flex", justifyContent: "center", width: "100%"}}>
     <Alert sx={{marginRight: "80px"}} severity="info">Number of Reviews: { feedbacks.length }</Alert>
-    <Alert severity="warning">Average Rating: 10</Alert>
+    <Alert severity="warning">Average Rating: { avgRating }</Alert>
     </Container>
   )
 }
